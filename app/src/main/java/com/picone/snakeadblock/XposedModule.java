9a140b192b385fa472622fdccefafda03f04567c
cj.he <cj.he@aftership.com>
2022-10-05 15:57:53 +0800

    feat:随机修改无尽模式分数，任务完成无压力

--- app/src/main/java/com/picone/snakeadblock/XposedModule.java
+++ app/src/main/java/com/picone/snakeadblock/XposedModule.java
@@ -4,15 +4,19 @@ import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
 import static de.robv.android.xposed.XposedHelpers.findClass;
 
 import android.app.Activity;
+import android.content.Context;
 import android.util.Log;
 import android.view.ViewGroup;
 
 import org.json.JSONObject;
 
+import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.util.HashMap;
+import java.util.Random;
 
 import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XC_MethodReplacement;
 import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
@@ -29,6 +33,7 @@ public class XposedModule implements IXposedHookLoadPackage {
         hideLittleGameAd(lpparam); // 小游戏，如转盘等广告
         hideSplashAd(lpparam); // 开屏广告
         hideVideoRewardAd(lpparam); // 奖励广告
+        modifySingleGameScore(lpparam); // 修改单机游戏的分数
     }
 
     private void hideSplashAd(XC_LoadPackage.LoadPackageParam lpparam) {
@@ -53,10 +58,14 @@ public class XposedModule implements IXposedHookLoadPackage {
             findAndHookMethod(cocosReqProcessorClazz, "showAd", Activity.class, String.class, String.class, new XC_MethodReplacement() {
                 @Override
                 protected Object replaceHookedMethod(MethodHookParam param) throws Throwable {
-                    Log.d(LOG_TAG, "on cocos req ad,arg1:" + param.args[1] + ",arg2:" + param.args[2]);
-                    JSONObject data = new JSONObject((String) param.args[2]);
-                    int seq = data.getInt("seq");
-                    publishResultMethod.invoke(null, true, "show_ad", seq);
+                    try {
+                        Log.d(LOG_TAG, "on cocos req ad,arg1:" + param.args[1] + ",arg2:" + param.args[2]);
+                        JSONObject data = new JSONObject((String) param.args[2]);
+                        int seq = data.getInt("seq");
+                        publishResultMethod.invoke(null, true, "show_ad", seq);
+                    } catch (Exception e) {
+                        Log.w(LOG_TAG, e);
+                    }
                     return null;
                 }
             });
@@ -79,11 +88,41 @@ public class XposedModule implements IXposedHookLoadPackage {
             findAndHookMethod("com.wepie.snake.module.a.c", lpparam.classLoader, "a", Activity.class, int.class, int.class, "com.wepie.ad.base.c", boolean.class, HashMap.class, new XC_MethodReplacement() {
                 @Override
                 protected Object replaceHookedMethod(MethodHookParam param) throws Throwable {
-                    Object listener = param.args[3];
-                    Log.d(LOG_TAG, "AdHelper show ad, listener:" + listener);
-                    listener.getClass().getDeclaredMethod("onSuccess", String.class).
-                            invoke(listener, "mimo_video");
+                    try {
+                        Object listener = param.args[3];
+                        Log.d(LOG_TAG, "AdHelper show ad, listener:" + listener);
+                        listener.getClass().getDeclaredMethod("onSuccess", String.class).
+                                invoke(listener, "mimo_video");
+                    } catch (Exception e) {
+                        Log.w(LOG_TAG, e);
+                    }
                     return null;
+                }
+            });
+        } catch (XposedHelpers.ClassNotFoundError | NoSuchMethodError e) {
+            Log.w(LOG_TAG, "can not find method:" + e);
+        } catch (Exception e) {
+            Log.w(LOG_TAG, e);
+        }
+    }
+
+    private void modifySingleGameScore(XC_LoadPackage.LoadPackageParam lpparam) {
+        try {
+            Class<?> modelClazz = findClass("com.wepie.snake.module.game.logic.a.b", lpparam.classLoader);
+            Field lengthField = modelClazz.getDeclaredField("a");
+            Field killField = modelClazz.getDeclaredField("b");
+            Field gameTimeField = modelClazz.getDeclaredField("e");
+            Field destroyLengthField = modelClazz.getDeclaredField("g");
+            Field collectField = modelClazz.getDeclaredField("j");
+            findAndHookMethod("com.wepie.snake.module.game.logic.b", lpparam.classLoader, "a", Context.class, "com.wepie.snake.module.game.logic.a.b", new XC_MethodHook() {
+                @Override
+                protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                    Random rnd = new Random();
+                    lengthField.setInt(param.args[1], 100000 + rnd.nextInt(3000000));
+                    gameTimeField.setInt(param.args[1], 300 + rnd.nextInt(100));
+                    killField.setInt(param.args[1], 10 + rnd.nextInt(100));
+                    destroyLengthField.setInt(param.args[1], 13000 + rnd.nextInt(100000));
+                    collectField.setInt(param.args[1], 260 + rnd.nextInt(10));
                 }
             });
         } catch (XposedHelpers.ClassNotFoundError | NoSuchMethodError e) {
