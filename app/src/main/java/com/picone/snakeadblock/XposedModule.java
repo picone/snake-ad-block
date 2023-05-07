62b78871b20b924236c289da59058ed008bf7bb2
cj.he <cj.he@aftership.com>
2023-05-07 18:58:02 +0800

    adapt for the latest version

--- app/src/main/java/com/picone/snakeadblock/XposedModule.java
+++ app/src/main/java/com/picone/snakeadblock/XposedModule.java
@@ -12,7 +12,10 @@ import org.json.JSONObject;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Objects;
 import java.util.Random;
 
 import de.robv.android.xposed.IXposedHookLoadPackage;
@@ -38,7 +41,7 @@ public class XposedModule implements IXposedHookLoadPackage {
 
     private void hideSplashAd(XC_LoadPackage.LoadPackageParam lpparam) {
         try {
-            findAndHookMethod("a.a.a.a.a.c.f.a", lpparam.classLoader, "a", ViewGroup.class, String.class, "com.miui.zeus.mimo.sdk.SplashAd.SplashAdListener", new XC_MethodReplacement() {
+            findAndHookMethod("c.a.a.a.a.c.f.a", lpparam.classLoader, "a", ViewGroup.class, String.class, "com.miui.zeus.mimo.sdk.SplashAd.SplashAdListener", new XC_MethodReplacement() {
                 @Override
                 protected Object replaceHookedMethod(MethodHookParam param) throws Throwable {
                     return null;
@@ -85,9 +88,9 @@ public class XposedModule implements IXposedHookLoadPackage {
 
     private void hideVideoRewardAd(XC_LoadPackage.LoadPackageParam lpparam) {
         try {
-            findAndHookMethod("com.wepie.snake.module.advertisement.ADHelper", lpparam.classLoader, "showVideo", Activity.class, int.class, int.class, "com.wepie.ad.base.IAdShowCallback", boolean.class, HashMap.class, new XC_MethodReplacement() {
+            findAndHookMethod("com.wepie.snake.module.advertisement.b", lpparam.classLoader, "a", Activity.class, int.class, int.class, "com.wepie.ad.base.c", boolean.class, HashMap.class, new XC_MethodReplacement() {
                 @Override
-                protected Object replaceHookedMethod(MethodHookParam param) throws Throwable {
+                protected Object replaceHookedMethod(MethodHookParam param) {
                     try {
                         Object callback = param.args[3];
                         callback.getClass().getDeclaredMethod("onSuccess", String.class).
@@ -107,13 +110,23 @@ public class XposedModule implements IXposedHookLoadPackage {
 
     private void modifySingleGameScore(XC_LoadPackage.LoadPackageParam lpparam) {
         try {
-            Class<?> modelClazz = findClass("com.wepie.snake.module.game.logic.entity.GameEndParam", lpparam.classLoader);
-            Field lengthField = modelClazz.getDeclaredField("length");
-            Field killField = modelClazz.getDeclaredField("kill");
-            Field gameTimeField = modelClazz.getDeclaredField("survivalTime");
-            Field destroyLengthField = modelClazz.getDeclaredField("destroyLength");
-            Field collectField = modelClazz.getDeclaredField("floaterCount");
-            findAndHookMethod("com.wepie.snake.module.game.logic.OffGameOverManager", lpparam.classLoader, "requestGameOverData", Context.class, modelClazz, "com.wepie.snake.net.http.handler.SendScoreHandler.Callback", new XC_MethodHook() {
+            Class<?> gameEndEntityClazz = findClass("com.wepie.snake.module.game.logic.a.d", lpparam.classLoader);
+            Field lengthField = gameEndEntityClazz.getDeclaredField("a");
+            Field killField = gameEndEntityClazz.getDeclaredField("b");
+            Field gameTimeField = gameEndEntityClazz.getDeclaredField("e");
+            Field destroyLengthField = gameEndEntityClazz.getDeclaredField("g");
+            Field collectField = gameEndEntityClazz.getDeclaredField("j");
+            Field activityField = gameEndEntityClazz.getDeclaredField("u");
+
+            Class<?> gameActivityEntityClazz = findClass("com.wepie.snake.module.game.logic.a.b", lpparam.classLoader);
+            Field activityIdField  = gameActivityEntityClazz.getDeclaredField("a");
+            Field collectListField  = gameActivityEntityClazz.getDeclaredField("b");
+
+            Class<?> gameActivityItemClazz = findClass("com.wepie.snake.module.game.logic.a.b.a", lpparam.classLoader);
+            Field activityItemId = gameActivityItemClazz.getDeclaredField("a");
+            Field activityItemNum = gameActivityItemClazz.getDeclaredField("b");
+
+            findAndHookMethod("com.wepie.snake.module.game.logic.b", lpparam.classLoader, "a", Context.class, gameEndEntityClazz, "com.wepie.snake.net.http.b.m$a", new XC_MethodHook() {
                 @Override
                 protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
                     Object endGameParam = param.args[1];
@@ -124,6 +137,41 @@ public class XposedModule implements IXposedHookLoadPackage {
                     destroyLengthField.setInt(endGameParam, 13000 + rnd.nextInt(100000));
                     if (collectField.getInt(endGameParam) > 0) {
                         collectField.setInt(endGameParam, 260 + rnd.nextInt(10));
+                    }
+                    // activity
+                    Object activity = activityField.get(endGameParam);
+                    if (activity != null) {
+                        Object activityId = activityIdField.get(activity);
+                        if (Objects.equals((String) activityId, "101")) {
+                            // 美食节活动，创建新的随机收集到的列表
+                            // 普通：12-25
+                            // 特别：101-104
+                            List<Object> newCollectList = new ArrayList<>();
+                            for (int i = 12; i <= 25; i++) {
+                                Object item = gameActivityItemClazz.newInstance();
+                                activityItemId.setInt(item, i);
+                                activityItemNum.setInt(item, 1+rnd.nextInt(30));
+                                newCollectList.add(item);
+                            }
+                            Object item = gameActivityItemClazz.newInstance();
+                            activityItemId.setInt(item, 101);
+                            activityItemNum.setInt(item, 1+rnd.nextInt(100));
+                            newCollectList.add(item);
+                            item = gameActivityItemClazz.newInstance();
+                            activityItemId.setInt(item, 102);
+                            activityItemNum.setInt(item, 1+rnd.nextInt(30));
+                            newCollectList.add(item);
+                            item = gameActivityItemClazz.newInstance();
+                            activityItemId.setInt(item, 103);
+                            activityItemNum.setInt(item, 1+rnd.nextInt(10));
+                            newCollectList.add(item);
+                            item = gameActivityItemClazz.newInstance();
+                            activityItemId.setInt(item, 104);
+                            activityItemNum.setInt(item, 1+rnd.nextInt(10));
+                            newCollectList.add(item);
+                            collectListField.set(activity, newCollectList);
+                            Log.w(LOG_TAG, "use new list, len=" + newCollectList.size());
+                        }
                     }
                 }
             });
